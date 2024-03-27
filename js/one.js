8bc82fae34131e9ed2bb9add2ef935a156e22f80
Vladimir <141544438+Soldizz@users.noreply.github.com>
2024-03-27 18:54:32 +0200

    js file created

---
+++ js/one.js
@@ -0,0 +1,15 @@
+const age = 15;
+console.log(age);
+const name = 'Sasha';
+console.log(name);
+const isStudent = true;
+console.log(isStudent);
+const myString = "im malenia blade of miquella";
+console.log(myString);
+let myNumber = 20;
+myNumber = + 10;
+console.log(myNumber);
+const myNull = null;
+console.log(myNull);
+const promptName = prompt('whats your name?');
+console.log("Hello", promptName);
