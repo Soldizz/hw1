cd7c7959c0b515689774e8d4610a3ac6d81654c5
Vladimir <141544438+Soldizz@users.noreply.github.com>
2024-04-01 17:36:44 +0300

    hw2

--- js/hw2.js
+++ js/hw2.js
@@ -1,11 +1,47 @@
-const colors = ["red", "black", "blue"];
-const min = 0;
-const max = colors.length - 1;
-const index = Math.round(Math.random() * (max - min) + min);
-console.log(index);
-const color = colors[index];
-console.log(color);
-document.body.style.backgroundColor = color;
+// const colors = ["red", "black", "blue"];
+// const min = 0;
+// const max = colors.length - 1;
+// const index = Math.round(Math.random() * (max - min) + min);
+// console.log(index);
+// const color = colors[index];
+// console.log(color);
+// document.body.style.backgroundColor = color;
 
 
 // hw2:
+const celsius = 24;
+const fahrenheit = celsius * 9 / 5 + 32;
+console.log(Math.round(fahrenheit));
+
+const days = 31;
+const hours = days * 24;
+const minutes = hours * 60;
+console.log(hours, minutes);
+
+const health = 10;
+const energy = 15;
+console.log(health - 2, energy - 4);
+
+const sum = 1220;
+const freebie = sum / 100 * 10;
+const money = sum - freebie;
+console.log(money);
+
+const number = 10.3;
+console.log(Math.floor(number));
+
+const fpNumbers = (1.3,1.5,1.7);
+console.log(Number.parseFloat(fpNumbers));
+
+const integer = ('10spsp');
+console.log(Number.parseInt(integer));
+
+const sqrtNumber = (100);
+console.log(Math.sqrt(sqrtNumber));
+
+const integerNumber = 10;
+const stringNumber = "20";
+const parsedInteger = parseInt(stringNumber);
+console.log(typeof parsedInteger); 
+const stringInteger = integerNumber.toString();
+console.log(typeof stringInteger); 
