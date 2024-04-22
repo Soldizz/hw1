2c51c4a5331556ded79fe69eb0aa13b3992d2ed5
Vladimir <141544438+Soldizz@users.noreply.github.com>
2024-04-22 18:38:22 +0300

    hw5

--- js/hw4.js
+++ js/hw4.js
@@ -1,10 +1,1 @@
 console.log("hw4:");
-// 1
-// const text1 = "aaa";
-// const text2 = "aaa";
-// if (text1 && text2 == Number) {
-//     console.log('1');
-// }
-// else {
-//     console.log("2");
-// }
--- js/hw5.js
+++ js/hw5.js
@@ -1,7 +1,103 @@
-const number = 15;
-console.log('global variable', number);
+console.log("hw5:");
 
-if (true) {
-    let number = 5;
-    console.log("global variable", number);
+let select = prompt('Кава,чай чи сік?');
+if (select == 'Кава') {
+    console.log(1);
+}
+else if (select == 'чай'){
+    console.log(2);
+}
+else{
+    console.log(3);
+}
+
+let day = prompt('Який сьогодні день за порядком?')
+switch (day) {
+    case 1:
+        console.log('вихідний');
+        break;
+    case 2:
+        console.log('вихідний');
+        break;
+    case 3:
+        console.log('вихідний');
+        break;
+    case 4:
+        console.log('вихідний');
+        break;
+    case 5:
+        console.log('вихідний');
+        break;
+    case 6:
+        console.log('робочий день');
+        break;
+    default:
+        console.log('робочий день');
+        break;
+}
+let monat = prompt("вкажіть номер місяця");
+switch (monat) {
+    case 1:
+        console.log("зима");
+        break;
+    case 2:
+        console.log("зима");
+        break;
+    case 3:
+        console.log("весна");
+        break;
+    case 4:
+        console.log("весна");
+        break;
+    case 5:
+        console.log("весна");
+        break;
+    case 6:
+        console.log("літо");
+        break;
+    case 7:
+        console.log("літо");
+        break;
+    case 8:
+        console.log("літо");
+        break;
+    case 9:
+        console.log("осінь");
+        break;
+    case 10:
+        console.log("осінь");
+        break;
+    case 11:
+        console.log("осінь");
+        break;
+    default:
+        console.log("зима");
+        break;
+}
+
+let color = prompt("червоний,зелений чи жовтий?");
+if (color == "червоний") {
+    console.log('стоп');
+} else if (color == "зелений") {
+    console.log('йти');
+} else {
+    console.log('зачекай');
+}
+
+let number1 = 10;
+let number2 = 5;
+let move = prompt("+,-,*,/")
+switch (move) {
+    case "+":
+        console.log(number1 + number2);
+        break;
+    case "-":
+        console.log(number1 - number2);
+        break;
+    case "*":
+        console.log(number1 * number2);
+        break;
+    default:
+        console.log(number1 / number2);
+        break;
 }
