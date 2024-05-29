37d0f8c1aac6f9c208bc855e5132c7d5fcea8bb1
Vladimir <141544438+Soldizz@users.noreply.github.com>
2024-05-29 18:29:09 +0300

    hw3

--- js/hw3.js
+++ js/hw3.js
@@ -1,3 +1,22 @@
-console.log("hw3:");
+
+let result = 5 + 5 + "5";
+console.log("Результат:", result);
+console.log("Тип змінної result:", typeof result);
+
+let email = "example@example.com";
+let containsAtSymbol = email.includes("@");
+let totalCharacters = email.length;
+console.log("Email містить символ @:", containsAtSymbol);
+console.log("Загальна кількість символів в email:", totalCharacters);
+
+let word1 = "My";
+let word2 = "name";
+let word3 = "is";
+let fullName = word1 + word2 +  word3 + "Viktor";
+console.log("fullName:", fullName);
+
+let userName = "Олександр";
+let payment = 300;
+alert('Дякуємо,' + userName + '.До сплати '+ payment + ' гривень!');
 
 
